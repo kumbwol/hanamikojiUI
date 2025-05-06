c409eb1612d07eae3a5a202bdc6d7d447168db9a
Jakab Wolosz <jakab.wolosz@oryxgaming.com>
2025-05-06 21:19:22 +0200

    add 1 command run

---
+++ run_all.py
@@ -0,0 +1,27 @@
+import subprocess
+import os
+import platform
+
+def run_command(command, cwd=None):
+    return subprocess.Popen(command, cwd=cwd, shell=True)
+
+processes = []
+
+# Start frontend
+processes.append(run_command("npm run start"))
+
+# Start filesystemIO (Node backend)
+processes.append(run_command("npm run filesystemIO"))
+
+# Run Python backend in the correct directory
+python_dir = os.path.join("engine", "hanamikoji")
+if platform.system() == "Windows":
+    activate_cmd = "..\\..\\venv\\Scripts\\activate && python engine.py"
+else:
+    activate_cmd = "source ../../venv/bin/activate && python engine.py"
+
+processes.append(run_command(activate_cmd, cwd=python_dir))
+
+# Wait for all
+for p in processes:
+    p.wait()
